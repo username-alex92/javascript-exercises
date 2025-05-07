33064f96438953e902cc5cfca29584fdee8bc0af
username-alex92 <80555709+username-alex92@users.noreply.github.com>
2025-05-08 01:16:01 +0300

    Complete task: 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,24 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+  const result = (degrees - 32) * 5 / 9;
+
+  if (Number.isInteger(result)) {
+    return result;
+  }
+
+  return Number(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+  const result = (degrees * 9 / 5) + 32;
+
+  if (Number.isInteger(result)) {
+    return result;
+  }
+
+  return Number(result.toFixed(1));
 };
+
+console.log(typeof convertToCelsius(-100));
 
 // Do not edit below this line
 module.exports = {
