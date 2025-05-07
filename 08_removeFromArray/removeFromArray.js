a1ee5a1c8f049a47a8c3426c31512f47d836a066
username-alex92 <80555709+username-alex92@users.noreply.github.com>
2025-05-07 21:18:08 +0300

    Complete task: 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...otherArgs) {
+    const returnArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (otherArgs.includes(arr[i])) {
+            continue;
+        } 
+
+        returnArr.push(arr[i]);
+    }
+
+    return returnArr;
 };
 
 // Do not edit below this line
