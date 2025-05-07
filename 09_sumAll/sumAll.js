de6ab570ab8d97fbfa94a7c956c3421e8906dbd6
username-alex92 <80555709+username-alex92@users.noreply.github.com>
2025-05-07 21:51:21 +0300

    Complete task: 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(start, end) { 
+    if ((start < 0 || end < 0) ||
+        (!Number.isInteger(start) || !Number.isInteger(end))) {
+            return "ERROR";
+        }
 
+    if (start > end) {
+        let helper = start;
+        start = end;
+        end = helper;
+    }
+
+    let sum = 0;
+
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
