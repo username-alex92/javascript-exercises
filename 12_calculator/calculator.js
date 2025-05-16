e67eb0e8c5d49e8189dad30fbc644da82734ffaf
username-alex92 <80555709+username-alex92@users.noreply.github.com>
2025-05-17 02:35:14 +0300

    Complete task: 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, currentValue) => sum + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((sum, currentValue) => sum * currentValue)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let factorial = 1;
+	for (let i = 1; i <= a; i++) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
