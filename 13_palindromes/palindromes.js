4fd2f691023cf28db2f7eb11c7bd093e62c25599
username-alex92 <80555709+username-alex92@users.noreply.github.com>
2025-05-17 03:13:41 +0300

    Complete task: 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let newStr = str.toLowerCase().replace(/[^A-Za-z0-9]/g, "");
+    let count = 0;
+    for (let i = newStr.length - 1; i >= 0; i--) {
+        if (newStr[count] !== newStr[i]) return false;
+        console.log("count: " + newStr[count]);
+        console.log("i: " + newStr[i]);
+        count++;
+    }
 
+    return true;
 };
 
 // Do not edit below this line
