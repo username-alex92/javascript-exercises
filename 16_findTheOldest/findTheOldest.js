195683f97e46f586688a5efe0db38eb411ce925d
username-alex92 <80555709+username-alex92@users.noreply.github.com>
2025-05-18 01:52:05 +0300

    Complete task: 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    arr.sort((a, b) => {
+        if (!a.hasOwnProperty("yearOfDeath")) {
+            return (new Date().getFullYear() - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth);
+        } 
+        if (!b.hasOwnProperty("yearOfDeath")) {
+            return (a.yearOfDeath - a.yearOfBirth) - (new Date().getFullYear() - b.yearOfBirth);
+        }
+        return (a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth);
+    });
 
+    return arr[arr.length - 1];
 };
 
 // Do not edit below this line
