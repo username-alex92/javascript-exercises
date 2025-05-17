84dbeecfc7da3589edb266b1dedf7cb269ea7ef0
username-alex92 <80555709+username-alex92@users.noreply.github.com>
2025-05-17 21:32:33 +0300

    Complete task: 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num == 0) return 0;
+    if (num < 0) return "OOPS";
 
+    const fibonacciArr = [1, 1];
+    for (let i = 2; i <= num; i++) {
+        let first = fibonacciArr[i - 2];
+        let second = fibonacciArr[i - 1];
+        fibonacciArr.push(first + second);
+    }
+
+    return fibonacciArr[num-1];
 };
 
 // Do not edit below this line
